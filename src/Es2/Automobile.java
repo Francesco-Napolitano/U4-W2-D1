a055aa746662a08f27526caf3f18bf3fa2674807
Francesco-Napolitano <francesco.46napolitano@gmail.com>
2025-01-13 15:46:05 +0100

    ese 2 non finito

--- src/Es2/Automobile.java
+++ src/Es2/Automobile.java
@@ -6,24 +6,26 @@ public class Automobile {
     private int distanza;
     private int litri;
 
-    public Automobile(int distanza, int litri) {
-        this.distanza = distanza;
-        this.litri = litri;
+    public Automobile() {
     }
 
-    public double divisione(int km, int l) {
-        return (double) km / l;
+    public double divisione() {
+        if (litri != 0) {
+            throw new ArithmeticException("Non è possibile la divisione per 0");
+        }
+        return (double) distanza / litri;
     }
 
-    public void rapporto () throws RapportoException{
-        try (Scanner myScanner = new Scanner(System.in)){
+    public void rapporto() throws RapportoException {
+        try (Scanner myScanner = new Scanner(System.in)) {
             distanza = myScanner.nextInt();
             litri = myScanner.nextInt();
-            if (litri != 0){
-                divisione(distanza,litri);
-            } else {
-                System.out.println("Non è possibile calcolare il rapporto.");
-            }
-        } catch ()
+            double rapporto = divisione();
+            System.out.println("Il rapporto è di "+  rapporto);
+        } catch (ArithmeticException e) {
+            System.err.println("Errore "+ e.getMessage());
+        }
     }
+
+
 }
